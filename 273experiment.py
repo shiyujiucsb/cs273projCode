aaff4dcf3bea79f0adc9216f35b1b0f62fbe0dc1
shiyujiucsb <shiyu@cs.ucsb.edu>
2016-10-20 02:17:22 -0700

    add ru

--- 273experiment.py
+++ 273experiment.py
@@ -7,7 +7,7 @@
 
 '''
 Exact solutions.
-Input: data set file name, # itemsets
+Input: dataset file name, # itemsets
 Output: list of itemset frequencies
 '''
 def calcExactFreqs(fname, nItemsets):
@@ -25,22 +25,22 @@ def calcExactFreqs(fname, nItemsets):
 
 '''
 Approximated solutions given sample size.
-Input: data set file name, # itemsets, # samples.
+Input: dataset file name, # itemsets, # samples.
 Output: list of approximated itemset frequencies
 '''
 def apprxFreqs(fname, nItemsets, nSamples):
     freqs = [0 for _ in range(nItemsets+1)]
     f = open(fname, 'r')
     lines = f.readlines()
+    f.close()
     nTransactions = len(lines)
     from random import randint
     for _ in range(nSamples):
-        line = lines[randint(1,nTransactions)]
+        line = lines[randint(0,nTransactions-1)]
         transaction = [int(i) for i in line.strip().split(' ')]
         for itemset in transaction:
             if 0 <= itemset <= nItemsets:
                 freqs[itemset] += 1
-    f.close()
     return list(map(lambda x:x*1.0/nSamples, freqs))
 
 '''
@@ -53,3 +53,50 @@ def worstError(freq1, freq2):
         return None
     return max([abs(freq1[i]-freq2[i]) for i in range(len(freq1))])
     
+'''
+Approximation with new bound.
+Input: dataset file name, # itemsets, epsilon, delta
+Output: approximated frequencies, # samples
+'''
+def newBoundApprox(fname, nItemsets, epsilon, delta):
+    f = open(fname, 'r')
+    nTransactions = 0
+    for _ in f: nTransactions += 1
+    f.close()
+    
+    from math import log
+    n = int((log(2*nItemsets)-log(delta))/(2*epsilon*epsilon))+1
+    if n > nTransactions:
+        return calcExactFreqs(fname, nItemsets), nTransactions
+    return apprxFreqs(fname, nItemsets, n), n
+    
+'''
+Classical approximation using old bound (RU15@KDD)
+Input: dataset file name, # itemsets, epsilon, delta
+Output: approximations, # samples
+'''
+def RUApprox(fname, nItemsets, epsilon, delta):
+    freqs = [0 for _ in range(nItemsets+1)]
+    f = open(fname, 'r')
+    lines = f.readlines()
+    nTransactions = len(lines)
+    f.close()
+    from random import randint
+    from math import log
+    n = 0
+    dn = int(2*log(2/delta)/(epsilon*epsilon))+1
+    while True:
+        for _ in range(n, n+dn):
+            line = lines[randint(0,nTransactions-1)]
+            transaction = [int(i) for i in line.strip().split(' ')]
+            for itemset in transaction:
+                if 0 <= itemset <= nItemsets:
+                    freqs[itemset] += 1
+        n = n+dn
+        ell = max(freqs)**0.5
+        Delta = 2*ell/n*(2*log(nItemsets))**0.5 + (2*log(2/delta)/n)**0.5
+        if Delta <= epsilon or n >= nTransactions:
+            break
+        dn = int(2*log(2/delta)/((epsilon-2*ell/n*(2*log(nItemsets))**0.5)**2))+1-n
+        dn = max(dn,1)
+    return list(map(lambda x:x*1.0/n,freqs)), n
